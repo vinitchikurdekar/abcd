c0e570ce04e9d0f94d3b37941dd56f7a369dd162
unknown <chikurdekarvinit5262@gmail.com>
2022-04-13 22:32:13 +0530

    deal done

---
+++ zerodha/src/main/java/baseclasses/Utility.java
@@ -0,0 +1,5 @@
+package baseclasses;
+
+public class Utility {
+
+}
---
+++ zerodha/src/main/java/configurations/Configuration.java
@@ -0,0 +1,12 @@
+package configurations;
+
+public class Configuration {
+	
+	public static String driverpath ="C:\\Users\\VINIT\\eclipse-workspace\\zerodha\\src\\main\\resources\\drivers\\chromedriver.exe";
+
+	public static String appUrl="https://www.facebook.com/signup";
+	
+	public static String screenshotPath="C:\\Users\\VINIT\\eclipse-workspace\\zerodha\\target\\screenshot";
+	
+	public static String excelpath="C:\\Users\\VINIT\\eclipse-workspace\\zerodha\\src\\test\\resources\\testdata\\Testdata.xlsx";
+}
